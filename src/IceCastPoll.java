f1842ddb4119210083cc002cf3b7cf07476ff2d0
Alfred Stump <officercartman@gmail.com>
2012-09-15 08:45:00 -0400

    added some logic

--- src/IceCastPoll.java
+++ src/IceCastPoll.java
@@ -11,7 +11,6 @@ import java.util.*;
 import java.lang.*;
 
 import net.moraleboost.streamscraper.ScrapeException;
-import org.jibble.pircbot.*;
 
 import java.net.URI;
 
@@ -20,44 +19,75 @@ import net.moraleboost.streamscraper.Scraper;
 import net.moraleboost.streamscraper.scraper.IceCastScraper;
 
 
-
 public class IceCastPoll extends TimerTask {
 
     private RadioBot bot;
-    private String channel = "#programming";
+    private String channel = "#cart";
     private boolean radioStatus = false;
+    private boolean topicChanged = false;
+    private String title = "";
+    private String currentSong = "";
+    private String pastSong = "";
+    private String description = "";
+    private String bitRate = "";
+    private String contentType = "";
+    private String genre = "";
+    private int currentListeners = 0;
+    private int peakListeners = 0;
 
     public IceCastPoll(RadioBot radioBot) {
         this.bot = radioBot;
     }
 
     public void run() {
-    Scraper scraper = new IceCastScraper();
-     String song = "";
-    try {
-        List<Stream> streams = scraper.scrape(new URI("http://radio.7chan.org:8000/radio"));
+        Scraper scraper = new IceCastScraper();
+        String song = "";
+        try {
+            List<Stream> streams = scraper.scrape(new URI("http://radio.7chan.org:8000/"));
 
-        for (Stream stream : streams) {
+            for (Stream stream : streams) {
 
-            if (stream.getTitle() == "radio7"){
-                radioStatus = true;
-            } else {
-                radioStatus = false;
+                if (stream.getTitle() == "radio7") {
+                    radioStatus = true;
+                    title = stream.getTitle();
+                    pastSong = song;
+                    song = stream.getCurrentSong();
+                    description = stream.getDescription();
+                    currentListeners = stream.getCurrentListenerCount();
+                    peakListeners = stream.getPeakListenerCount();
+                    bitRate = stream.getBitRate();
+                    contentType = stream.getContentType();
+                    genre = stream.getGenre();
+                } else {
+                    radioStatus = false;
+                }
+            }
 
+        } catch (URISyntaxException e) {
+
+            System.out.println(e.getMessage());
+        } catch (ScrapeException e) {
+
+            System.out.println(e.getMessage());
+        }
+
+        if (radioStatus == true) {
+            if (topicChanged = false) {
+                bot.setTopic(channel, description + " is playing " + genre + ". Listen via http://radio.7chan.org:8000/radio.m3u");
+                topicChanged = true;
+            }
+            if (pastSong != song) {
+                bot.sendMessage(channel, song + ", " + currentListeners + "/" + peakListeners);
+            }
+        } else {
+            bot.listChannels(channel);
+            if (bot.currentTopic != "OFF-AIR || http://radio.7chan.org:8000/radio.m3u") {
+                bot.setTopic(channel, "OFF-AIR || http://radio.7chan.org:8000/radio.m3u");
             }
 
         }
     }
-    catch (URISyntaxException e) {
 
-        System.out.println(e.getMessage());
-    }
-    catch (ScrapeException e) {
-
-        System.out.println(e.getMessage());
-    }
-
-    }
 
 }
 
